4cec28d5558b8d519fc8da4fd02b9c118c68ccc4
theclassicwizard <srkjadara@gmail.com>
2020-12-07 17:39:12 +0530

    Map with dataset!

--- client/src/components/MapChart.js
+++ client/src/components/MapChart.js
@@ -4,13 +4,24 @@
 import React, { useState, useEffect } from "react"
 import { geoMercator, geoPath } from "d3-geo"
 import { feature } from "topojson-client"
+import {csv} from "d3-fetch"
 
 const projection = geoMercator()
+
+let districtsData = []
+
+const setData = data => {
+  districtsData = data.map((dist) => dist);
+}
+
 
 const MapChart = () => {
   const [geographies, setGeographies] = useState([])
 
   useEffect(() => {
+    csv("/districts_data_v0.csv").then(districts => {
+      setData(districts);
+    })
     fetch("/District_Boundary.json")
       .then(response => {
         if (response.status !== 200) {
@@ -27,16 +38,19 @@ const MapChart = () => {
     <svg width={ 800 } height={ 800 } viewBox="0 0 800 800">
       <g className="districts">
         {
-          geographies.map((d,i) => (
-            <path
-              key={ `path-${ i }` }
-              d={ geoPath().projection(projection)(d) }
-              className="district"
-              fill={ `rgba(50,50,56,${ 1 / geographies.length * i})` }
-              stroke="#000000"
-              strokeWidth={ 0.5 }
-            />
-          ))
+          geographies.map((d,i) => { 
+            const cur = districtsData.find(s => s.district_id == i+1); 
+            return (
+              <path
+                key={ `path-${ i }` }
+                d={ geoPath().projection(projection)(d) }
+                className="district"
+                fill={ `rgba(138,43,226,${ cur.samples / 1000})` }
+                stroke="#000000"
+                strokeWidth={ 1.0 }
+              />
+            )}
+          )
         }
       </g>
     </svg>
